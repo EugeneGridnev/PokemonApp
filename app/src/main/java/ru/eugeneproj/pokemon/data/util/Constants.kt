b8fffc4f4f6e63ecd6adf4df35c04e3271b4eb25
EugeneGridnev <eugenegrid@yandex.ru>
2024-05-11 16:27:15 +0300

    di added

--- app/src/main/java/ru/eugeneproj/pokemon/data/util/Constants.kt
+++ app/src/main/java/ru/eugeneproj/pokemon/data/util/Constants.kt
@@ -7,7 +7,7 @@ object Constants {
     const val BASE_URL = "https://pokeapi.co/api/v2"
     const val PAGE_SIZE = 20
 
-    //нужно чтобы пэйджинг3 грузил первично по 20 станиц
+    //нужно чтобы пэйджинг3 грузил первично по 20
     private const val PREFETCH_DISTANCE = PAGE_SIZE / 2
 
     val PAGING_CONFIG = PagingConfig(
