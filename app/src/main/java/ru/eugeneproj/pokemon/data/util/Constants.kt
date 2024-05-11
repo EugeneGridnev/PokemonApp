b3dd85b15d3447e89757cdcf2092e1272dfd9c11
EugeneGridnev <eugenegrid@yandex.ru>
2024-05-11 16:09:41 +0300

    constants util added

---
+++ app/src/main/java/ru/eugeneproj/pokemon/data/util/Constants.kt
@@ -0,0 +1,18 @@
+package ru.eugeneproj.pokemon.data.util
+
+import androidx.paging.PagingConfig
+
+object Constants {
+
+    const val BASE_URL = "https://pokeapi.co/api/v2"
+    const val PAGE_SIZE = 20
+
+    //нужно чтобы пэйджинг3 грузил первично по 20 станиц
+    private const val PREFETCH_DISTANCE = PAGE_SIZE / 2
+
+    val PAGING_CONFIG = PagingConfig(
+        pageSize = PAGE_SIZE,
+        prefetchDistance = PREFETCH_DISTANCE,
+        enablePlaceholders = false
+    )
+}
